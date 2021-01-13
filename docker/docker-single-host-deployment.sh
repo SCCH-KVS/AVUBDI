2264185dd964f181ccb43653905e9708ce03be08
mmayr96 <mayr.michael11@gmail.com>
2021-01-13 18:27:20 +0100

    Fix Host Deployment of Infrastructure

--- docker/docker-single-host-deployment.sh
+++ docker/docker-single-host-deployment.sh
@@ -3,6 +3,4 @@
 # NOTE: This file deploys the infrastructure services on the local system with an overlay network.
 # Make sure to have Docker installed and running! For Windows download Docker Desktop and switch to Linux containers.
 # If your are using Ubuntu or REHL distributions, you can use "docker-install-RHEL.sh" or "docker-install-Ubuntu.sh" 
-scp -r ./cogniplant root@10.0.0.1:/home
-
-ssh root@10.0.0.1 "docker-compose up /home/cogniplant/docker-compose.yml -d --build"
+docker-compose up -d --build
--- docker/docker-swarm-multiple-hosts-deployment.sh
+++ docker/docker-swarm-multiple-hosts-deployment.sh
@@ -20,12 +20,11 @@ ssh root@10.0.0.1 "docker node update --label-add role=analytics 10.0.0.2"
 ssh root@10.0.0.1 "docker node update --label-add role=batch_processing 10.0.0.3"
 ssh root@10.0.0.1 "docker node update --label-add role=stream_processing 10.0.0.4"
 
-# NOTE: this is only an example file on how to deploy the infrastructure code to the virtual machines, that have the docker deamon installed and running and act as swarm nodes.
-# 10.0.0.1 acts as the head node while the other nodes (10.0.0.2-10.0.0.4) are for processing and visual analytics. 
-scp -r ./cogniplant root@10.0.0.1:/home #master (kafka + UI,zookeeper,kafka-connect + UI, portainer, schema-registry + UI)
-scp -r ./cogniplant root@10.0.0.2:/home #visual analytics (grafana, chronograf)
-scp -r ./cogniplant root@10.0.0.3:/home #batch (Spark, Spark Streaming, MLFlow)
-scp -r ./cogniplant root@10.0.0.4:/home #stream (Spark, Spark Streaming, MLFlow)
+# NOTE: copy git repository to every swarm node bevor continuing, preferably under /home/
+ssh root@10.0.0.1 "git clone https://github.com/SCCH-KVS/AVUBDI/ /home"
+ssh root@10.0.0.2 "git clone https://github.com/SCCH-KVS/AVUBDI/ /home"
+ssh root@10.0.0.3 "git clone https://github.com/SCCH-KVS/AVUBDI/ /home"
+ssh root@10.0.0.4 "git clone https://github.com/SCCH-KVS/AVUBDI/ /home"
 
 # Infrastructure Deployment to the Swarm
-ssh root@10.0.0.1 "docker stack deploy --compose-file /home/cogniplant/docker-compose.yml cogniplant"
+ssh root@10.0.0.1 "docker stack deploy --compose-file /home/AVUBDI/docker/cogniplant/docker-compose.yml cogniplant"
